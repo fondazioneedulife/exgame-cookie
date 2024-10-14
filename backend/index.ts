b3c808b08df91da330081dc88c2ffb732e020641
Talhaimran03 <imrantalha199@gmail.com>
2024-10-14 11:34:28 +0200

    add tracherRouters to index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ import { createServer } from "http";
 import Koa from "koa";
 import serveStatic from "koa-static";
 import { initSocketIo } from "./io";
+import tracherRouters from "./routes/teachers";
 
 const app = new Koa();
 const router = new Router();
@@ -15,18 +16,13 @@ app.use(cors()); // TODO: configure for production
 app.use(bodyParser());
 app.use(serveStatic(`./public`, {}));
 
-app.use((ctx, next) => {
-  console.log("Incoming HTTP request");
-  // ctx.status = 200;
-  // ctx.body = "Hello Koa";
-  next();
-});
-
 router.get("/", (ctx) => {
-  ctx.body = "Ciao";
+  ctx.body = "ExGame - school is fun";
 });
 
 app.use(router.routes()).use(router.allowedMethods());
+
+app.use(tracherRouters.routes()).use(tracherRouters.allowedMethods());
 
 httpServer.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.HOST}:${process.env.PORT}`);
