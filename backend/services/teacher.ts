1bd16a046fc90899dc8b960a3b33db4a7210e4f1
Talhaimran03 <imrantalha199@gmail.com>
2024-10-14 11:32:51 +0200

    add techer services

---
+++ backend/services/teacher.ts
@@ -0,0 +1,34 @@
+import { Teacher } from "../../api-types/teacher";
+
+const DB: Teacher[] = [];
+
+export const index = async (): Promise<Teacher[]> => {
+  return DB;
+};
+
+export const view = async (id: string): Promise<Teacher | null> => {
+  return DB.find((teacher) => teacher._id === id) || null;
+};
+
+export const add = async (teacher: Teacher): Promise<void> => {
+  DB.push(teacher);
+};
+
+export const edit = async (
+  id: string,
+  teacher: Teacher,
+): Promise<Teacher | null> => {
+  const index = DB.findIndex((teacher) => teacher._id === id);
+  if (index === -1) {
+    return null;
+  }
+  DB[index] = teacher;
+  return teacher;
+};
+
+export const remove = async (id: string): Promise<void> => {
+  const index = DB.findIndex((teacher) => teacher._id === id);
+  if (index !== -1) {
+    DB.splice(index, 1);
+  }
+};
