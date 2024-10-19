50ebbe6dfbfdc69ebcd2fa1d0f9d921dca2835ac
Alessandro Falezza <alessandro.falezza@intesys.it>
2024-10-19 16:54:36 +0200

    Teacher type: use singular form for describing image

--- api-types/teacher.ts
+++ api-types/teacher.ts
@@ -1,9 +1,9 @@
 export type Teacher = {
-    _id: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    courses?: string[];
-    images?: string;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  courses?: string[];
+  image?: string;
 };
