de26304927975f2c530d94f1d763766577f87515
Tommaso <tommasococco02@gmail.com>
2024-10-28 12:57:42 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,6 +49,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
+    
   );
 }
 
--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,59 @@
+import Table from "@mui/joy/Table";
+import Button from '@mui/joy/Button';
+import { Stack } from "@mui/joy";
+
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
+  return (
+  <>
+  
+    <Table aria-label="basic table">
+      <thead >
+          <Stack direction="row">
+            <th>Lista esami</th>
+            <Button >Nuovo esame</Button>
+          </Stack>
+        </thead>
+       <tbody> 
+        <tr>
+          <td>Esame</td>
+          <td>Classi</td>
+          <td>Azioni</td>
+        </tr>
+      
+        <tr>
+          <td>React</td>
+          <td>Classe E</td>
+          <td>
+            <Stack direction="row"  spacing={0.5}>
+              <Button size="sm">Modifica</Button>
+              <Button variant="outlined" size="sm">Sessioni</Button>
+              </Stack>
+          </td>
+        </tr>
+        <tr>
+          <td>Javascript</td>
+          <td>Classe B</td>
+          <td>
+            <Stack direction="row"  spacing={0.5}>
+              <Button size="sm">Modifica</Button>
+              <Button variant="outlined" size="sm">Sessioni</Button>
+              </Stack>
+          </td>
+        </tr>
+        <tr>
+          <td>Node</td>
+          <td>classe A</td>
+          <td>
+            <Stack direction="row"  spacing={0.5}>
+              <Button size="sm">Modifica</Button>
+              <Button variant="outlined" size="sm">Sessioni</Button>
+              </Stack>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+
+
+  </>);
 };
