6c037142836575a51a3b52ab087e48c89e5f1e7f
GianMarco092003 <150621738+GianMarco092003@users.noreply.github.com>
2024-10-28 12:49:59 +0100

    lezione5

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,52 @@
+import { Button, Table } from "@mui/joy";
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
+  return (
+    <>
+    <h1>Exams</h1>
+    <Button>Aggiungi Esame</Button>
+    <Table aria-label="basic table">
+      <thead>
+        <tr>
+          <th style={{ width: '20%' }}>Esami</th>
+          <th></th>
+          <th>corsi</th>
+          <th>Azioni</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>esame1</td>
+          <td>classe1, classe2</td>
+          <td><Button>Modifica</Button></td>
+          <td><Button>Sessioni</Button></td>
+        </tr>
+        <tr>
+          <td>esame2</td>
+          <td>classe1, class2</td>
+          <td><Button>Modifica</Button></td>
+          <td><Button>Sessioni</Button></td>
+        </tr>
+        <tr>
+          <td>esame3</td>
+          <td>classe1, class2</td>
+          <td><Button>Modifica</Button></td>
+          <td><Button>Sessioni</Button></td>
+        </tr>
+        <tr>
+          <td>esame4</td>
+          <td>classe1, class2</td>
+          <td><Button>Modifica</Button></td>
+          <td><Button>Sessioni</Button></td>
+        </tr>
+        <tr>
+          <td>esame5</td>
+          <td>classe1, class2</td>
+          <td><Button>Modifica</Button></td>
+          <td><Button>Sessioni</Button></td>
+        </tr>
+      </tbody>
+    </Table>
+    </>
+  );
 };
