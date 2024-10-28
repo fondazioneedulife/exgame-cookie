60cb212a83aa74dee778510567ed3fc260a600c2
DiegoM1111 <diegomilli03@gmail.com>
2024-10-28 12:58:45 +0100

    lezione 5

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,58 @@
+import * as React from 'react';
+import Table from '@mui/joy/Table';
+import Button from '@mui/joy/Button';
+import Stack from '@mui/joy/Stack';
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
+  return (
+    <>
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+        }}
+      >
+        <h1>Esami</h1>
+        <Button>aggiungi esame</Button>
+      </Stack>
+
+      <Table aria-label="basic table">
+        <thead>
+          <tr>
+            <th style={{ width: '30%' }}>Titolo</th>
+            <th style={{ width: '40%' }}>Classi</th>
+            <th style={{ width: '30%' }}>Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>React</td>
+            <td>Cookie</td>
+            <td>
+              <Button style={{marginRight: "1rem"}}>modifica</Button>
+              <Button>sessioni</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>Mongo db</td>
+            <td>Cookie, Pixel</td>
+          <td>
+              <Button style={{marginRight: "1rem"}}>modifica</Button>
+              <Button>sessioni</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>Cybersecurity</td>
+            <td>Cookie, Suse,Pixel, Zorin</td>
+          <td>
+              <Button style={{marginRight: "1rem"}}>modifica</Button>
+              <Button>sessioni</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 };
