bc1fed287bd39fffe09586c065d1422b4ce87662
annabenettoni <annabenettoni@gmail.com>
2024-10-28 12:59:32 +0100

    exam first page draft

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,57 @@
+import { Table } from "@mui/joy";
+import { EditButton } from "./examsButtons/EditButton";
+import { SessionsButton } from "./examsButtons/SessionsButton";
+import classes from "./exam.module.css";
+import { AddButton } from "./examsButtons/AddButton";
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
+  return (
+    <>
+      <div className={classes.headerLayout}>
+        <h1 style={{ padding: 0, margin: 0 }}>Esami</h1>
+        <AddButton></AddButton>
+      </div>
+      <Table aria-label="basic table">
+        <thead>
+          <tr>
+            <th style={{ width: "40%" }}>Esami</th>
+            <th style={{ width: "40%" }}>Classi</th>
+            <th style={{ width: "40%" }}>Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>React</td>
+            <td>Pixel</td>
+            <td>
+              <div className={classes.buttonsLayout}>
+                <EditButton />
+                <SessionsButton />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Node.js</td>
+            <td>Suse</td>
+            <td>
+              <div className={classes.buttonsLayout}>
+                <EditButton />
+                <SessionsButton />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Mongo</td>
+            <td>Cookie</td>
+            <td>
+              <div className={classes.buttonsLayout}>
+                <EditButton />
+                <SessionsButton />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 };
