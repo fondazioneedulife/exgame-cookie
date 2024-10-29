593ced660051c77eb6dc077ecb5d285494ef2117
Matteo Scandolara <matteo.scando@gmail.com>
2024-10-29 09:22:15 +0100

    creazione tabella per visualizzazione esami

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,71 @@
+import Table from '@mui/joy/Table';
+import Button from '@mui/joy/Button';
+import { Stack, Chip } from '@mui/joy';
+import Typography from '@mui/joy/Typography';
+
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
+  return (
+    <>
+    <Stack direction="row" spacing={1}
+      sx={{
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 2,
+      }}
+    >
+      <Typography level="h2">Esami</Typography>
+      <Button variant="solid" onClick={function(){}}>Crea esame</Button>
+
+    </Stack> 
+
+      <Table aria-label="basic table">
+        <thead>
+          <tr>
+            <th><Typography level="h4">Titolo</Typography></th>
+            <th><Typography level="h4" sx={{ textAlign: 'center' }}>Classi</Typography></th>
+            <th><Typography level="h4" sx={{ textAlign: 'center' }}>Azioni</Typography></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><Typography level="title-md">Titolo</Typography></td>
+            <td>
+              <Stack direction="row" spacing={1}
+                  sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Chip>Cookie</Chip> <Chip>Suse</Chip>
+              </Stack>
+            </td>
+            <td>
+              <Stack direction="row" spacing={1}
+                sx={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  color="neutral"
+                  onClick={function(){}}
+                  size="md"
+                  variant="outlined"
+                > Modifica </Button>
+
+                <Button
+                  color="neutral"
+                  onClick={function(){}}
+                  size="md"
+                  variant="outlined"
+                > Sessioni </Button>
+
+              </Stack>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 };
