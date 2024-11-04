3f27d8e63b3e587ca0ad717e80826c15557a782c
filippomeneguzzi <filippomeneguzzi30@gmail.com>
2024-11-04 11:55:01 +0100

    creazione file routing

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,57 @@
+import Table from '@mui/joy/Table';
+import Stack from '@mui/joy/Stack';
+import Button from '@mui/joy/Button';
+
+
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
-};
+  return (
+        <Table aria-label="basic table">
+          <thead>
+            <tr>
+              <th style={{ width: '40%' }}>Esame</th>
+              <th>Classe</th>
+              <th>Modifica</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>JavaScript</td>
+              <td>Mouse</td>
+              <td><Stack direction="row" spacing={1}>
+                <Button>Modifica</Button>
+                <Button
+                  sx={{
+                    backgroundColor: 'success.outlinedColor',
+                    "&:hover":{backgroundColor: 'success.outlinedColor'}
+                  }}>Sessioni</Button>
+            </Stack></td>
+            </tr>
+            <tr>
+              <td>HTML e CSS</td>
+              <td>Cookie</td>
+              <td><Stack direction="row" spacing={1}>
+                <Button>Modifica</Button>
+                <Button
+                  sx={{
+                    backgroundColor: 'success.outlinedColor',
+                    "&:hover":{backgroundColor: 'success.outlinedColor'}
+                  }}>Sessioni</Button>
+            </Stack></td>
+            </tr>
+            <tr>
+              <td>React</td>
+              <td>Cookie</td>
+              <td><Stack direction="row" spacing={1}>
+                <Button>Modifica</Button>
+                <Button
+                  sx={{
+                    backgroundColor: 'success.outlinedColor',
+                    "&:hover":{backgroundColor: 'success.outlinedColor'}
+                  }}>Sessioni</Button>
+            </Stack></td>
+            </tr>
+          </tbody>
+        </Table>
+      );
+    }
