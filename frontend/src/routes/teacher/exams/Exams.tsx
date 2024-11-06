8a91c5f3ff2246d2307458a1ee2a02dc43b28a5c
Lorenzo Russo <lorenzo.2000.russo@gmail.com>
2024-11-06 12:21:31 +0100

    update exams.tsx

--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,3 +1,89 @@
+import { Table, Typography, Button, Stack, Chip } from "@mui/joy";
+
 export const Exams: React.FC = () => {
-  return <>Teacher exams</>;
+  return (
+    <>
+      <Table aria-label="basic table">
+        <thead>
+          <tr>
+            <th style={{ width: "40%" }}>
+              <Typography level="h1">Esami</Typography>
+            </th>
+            <th>Classi</th>
+            <th>Azioni</th>
+            <th>
+              <Button>Nuovo Esame</Button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Node.js</td>
+            <td>
+              <div>
+                <Stack direction="row" spacing={1}>
+                  <Chip>Cookie</Chip>
+                  <Chip>Pixel</Chip>
+                </Stack>
+              </div>
+            </td>
+            <td>
+              {" "}
+              <div>
+                <Stack direction="row" spacing={1}>
+                  <Button>Modifica</Button>
+                  <Button>Sessioni</Button>
+                </Stack>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="solid"
+          sx={{ borderRadius: "50%", backgroundColor: "primary" }}
+        >
+          1
+        </Button>
+        <Button
+          variant="solid"
+          sx={{ borderRadius: "50%", backgroundColor: "primary" }}
+        >
+          2
+        </Button>
+        <Button
+          variant="solid"
+          sx={{ borderRadius: "50%", backgroundColor: "primary" }}
+        >
+          3
+        </Button>
+        <Button
+          variant="solid"
+          sx={{ borderRadius: "50%", backgroundColor: "primary" }}
+        >
+          4
+        </Button>
+        <Button
+          variant="solid"
+          sx={{ borderRadius: "50%", backgroundColor: "primary" }}
+        >
+          5
+        </Button>
+        <Button
+          variant="solid"
+          sx={{ borderRadius: "50%", backgroundColor: "primary" }}
+        >
+          6
+        </Button>
+      </Stack>
+    </>
+  );
 };
