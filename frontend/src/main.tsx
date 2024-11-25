f28e60b42fe2863db9b8f6ce921040598749f5ff
Alessandro Falezza <alessandro.falezza@intesyt.it>
2024-11-25 10:33:24 +0100

    Remove duplicate variable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,13 +8,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {
-  extendTheme as materialExtendTheme,
-  CssVarsProvider as MaterialCssVarsProvider,
-  THEME_ID as MATERIAL_THEME_ID,
-} from "@mui/material/styles";
-
-const materialTheme = materialExtendTheme();
 
 const materialTheme = materialExtendTheme();
 
