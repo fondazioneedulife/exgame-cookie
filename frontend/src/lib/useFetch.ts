908eda337beec39093fd591647cb4d042c220046
Alessandro Falezza <alessandro.falezza@intesyt.it>
2024-11-25 13:18:32 +0100

    doc

--- frontend/src/lib/useFetch.ts
+++ frontend/src/lib/useFetch.ts
@@ -6,6 +6,16 @@ export type AuthenticatedFetchFunction = (
   init?: RequestInit,
 ) => Promise<void | Response>;
 
+/**
+ * Utilizza questo hook per invocare le api. Gestisce correttamente l'autenticazione e il redirect alla pagina di login.
+ *
+ * Utilizzo
+ * All'interno di un componente react, istanzia la variabile "fetch = useFetch()" e poi invoca le api con la stessa sintassi della funzione "fetch" standard. Esempio:
+ * ```js
+ * const fetch = useFetch();
+ * fetch('/api/to/fetch', { options });
+ * ```
+ */
 export const useFetch = (): AuthenticatedFetchFunction => {
   const navigate = useNavigate();
 
