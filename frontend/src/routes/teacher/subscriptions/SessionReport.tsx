735d0a4e683e75aed5217b9700c5d454f84c617d
anna.benettoni <anna.benettoni@intesys.it>
2024-11-25 12:50:02 +0100

    add single session details

--- frontend/src/routes/teacher/subscriptions/SessionReport.tsx
+++ frontend/src/routes/teacher/subscriptions/SessionReport.tsx
@@ -1,3 +1,30 @@
+import { Box, Table } from "@mui/joy";
+import { StudentSubscriptionRow } from "./StudentSubscriptionRow";
+
 export const SessionReport: React.FC = () => {
-  return <>Tutti gli esami di una sessione</>;
+  const student = [
+    { first_name: "Marco", last_name: "Bosco", results: "6/10" },
+    { first_name: "Anna", last_name: "Bosco", results: "6/10" },
+    { first_name: "Yasmine", last_name: "Bosco", results: "6/10" },
+  ];
+  return (
+    <>
+      <h1>Storia: sessione del 24/10/2024</h1>
+      <h2>Pixel</h2>
+
+      <Box></Box>
+
+      <Table>
+        <tr>
+          {student.map((value) => (
+            <StudentSubscriptionRow
+              first_name={value.first_name}
+              last_name={value.last_name}
+              results={value.results}
+            ></StudentSubscriptionRow>
+          ))}
+        </tr>
+      </Table>
+    </>
+  );
 };
