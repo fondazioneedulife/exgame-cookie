55041bde1c485c3f5f1bb0688e74e96a3471998e
Tommaso <tommasococco02@gmail.com>
2024-12-02 12:34:47 +0100

    fix import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import {
 } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.ts"
+import App from "./App"
 import "./index.css";
 
 const materialTheme = materialExtendTheme();
