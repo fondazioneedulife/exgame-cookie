b2ddcf043740e8eb5944fdfc2417c36e9007602e
Tommaso <tommasococco02@gmail.com>
2024-12-02 12:20:10 +0100

    allineamento master edulife

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-// App.js
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./components/Layout";
 import { config } from "./config";
@@ -11,14 +10,9 @@ import { StudentGuard } from "./routes/student/StudentGuard";
 import { StudentProfile } from "./routes/student/StudentProfile";
 import { MySubscriptions } from "./routes/student/subscriptions/MySubscriptions";
 import { Subscribe } from "./routes/student/subscriptions/Subscribe";
-<<<<<<< HEAD
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-=======
 import { TeacherGuard } from "./routes/teacher/TeacherGuard";
 import { Classes } from "./routes/teacher/classes/Classes";
 import { ClassStudents } from "./routes/teacher/classes/Students";
-import { AddExam } from "./routes/teacher/exams/AddExam";
->>>>>>> refs/remotes/origin/master
 import { EditExam } from "./routes/teacher/exams/EditExam";
 import { Exams } from "./routes/teacher/exams/Exams";
 import { Sessions } from "./routes/teacher/sessions/Sessions";
@@ -26,61 +20,7 @@ import { NewSession } from "./routes/teacher/sessions/sessionComponents/NewSessi
 import { SessionReport } from "./routes/teacher/subscriptions/SessionReport";
 import { SubscriptionReport } from "./routes/teacher/subscriptions/SubscriptionReport";
 import { Subscriptions } from "./routes/teacher/subscriptions/Subscriptions";
-<<<<<<< HEAD
-import { TeacherGuard } from "./routes/teacher/TeacherGuard";
 import AddExam from "./routes/teacher/exams/AddExam";
-
-function AnimatedRoutes() {
-  const location = useLocation();
-
-  return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.key}
-        timeout={300}
-        classNames="page"
-      >
-        <Routes location={location}>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Root />} />
-
-            {/* Teacher */}
-            <Route path="teacher" element={<TeacherGuard />}>
-              <Route index element={<Exams />} />
-              <Route path="exam" element={<AddExam />} />
-              <Route path="exam/:id" element={<EditExam />} />
-              <Route path="subscriptions" element={<Subscriptions />} />
-              <Route path="subscriptions/:date" element={<SessionReport />} />
-              <Route
-                path="subscriptions/:date/:id"
-                element={<SubscriptionReport />}
-              />
-            </Route>
-
-            {/* Student */}
-            <Route path="student" element={<StudentGuard />}>
-              <Route index element={<MySubscriptions />} />
-              <Route path="subscribe/:id" element={<Subscribe />} />
-              <Route
-                path="subscriptions/:date/:id"
-                element={<SubscriptionReport />}
-              />
-            </Route>
-
-            {/* Login */}
-            <Route path="login" element={<Login />} />
-          </Route>
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter basename="/">
-      <AnimatedRoutes />
-=======
 
 function App() {
   return (
@@ -126,7 +66,6 @@ function App() {
           <Route path="register" element={<Register />} />
         </Route>
       </Routes>
->>>>>>> refs/remotes/origin/master
     </BrowserRouter>
   );
 }
--- frontend/src/routes/teacher/exams/Exams.tsx
+++ frontend/src/routes/teacher/exams/Exams.tsx
@@ -1,76 +1,11 @@
-<<<<<<< HEAD
-import Table from "@mui/joy/Table";
-import Button from '@mui/joy/Button';
-import { useNavigate } from 'react-router-dom';
-import classes from './exams.module.css'
-import { useState } from 'react';
-import SingleExam from './examComponents/SingleExam';
-=======
 import Button from "@mui/joy/Button";
 import Stack from "@mui/joy/Stack";
 import Table from "@mui/joy/Table";
 import { useNavigate } from "react-router";
->>>>>>> refs/remotes/origin/master
 
 export const Exams: React.FC = () => {
   const navigate = useNavigate();
 
-<<<<<<< HEAD
-  // Stato per gestire la lista degli esami
-  const [exams, setExams] = useState([
-    { id: '1', name: 'Javascript', className: 'Classe B', date: '2024-11-15' },
-    { id: '2', name: 'Node', className: 'Classe A', date: '2024-12-01' },
-    { id: '3', name: 'Node', className: 'Classe A', date: '2024-12-01' },
-    { id: '4', name: 'Node', className: 'Classe A', date: '2024-12-01' },
-
-    // Altri esami possono essere aggiunti qui
-  ]);
-
-  // Funzione per eliminare un esame
-  const handleDelete = (id: string) => {
-    const updatedExams = exams.filter(exam => exam.id !== id);
-    setExams(updatedExams);
-  };
-
-  return (
-    <>
-      <div className={classes.container}>
-        <div className={classes.container_box}>
-          <div className={classes.container_table}>
-            <Button variant="outlined" size="sm" onClick={() => navigate('/teacher/exam')}>Aggiungi esame</Button>
-          </div>
-
-          <div className={classes.container_table}>
-            <Table aria-label="basic table">
-              <thead>
-                <tr>
-                  <th style={{ width: '40%' }}>Elenco esami</th>
-                  <th>Classe</th>
-                  <th>Azioni</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {/* Mappiamo ogni esame e lo passiamo al componente SingleExam */}
-                {exams.map(exam => (
-                  <SingleExam
-                    key={exam.id}
-                    id={exam.id}
-                    name={exam.name}
-                    className={exam.className}
-                    date={exam.date}
-                    onDelete={handleDelete}
-                  />
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-=======
   return (
     <div>
       <Button onClick={() => navigate("/teacher/exam")}>Aggiungi esame</Button>
@@ -140,4 +75,3 @@ export const Exams: React.FC = () => {
     </div>
   );
 };
->>>>>>> refs/remotes/origin/master
