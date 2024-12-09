63275d508cd2e4db975b62ca82e98aa00177cef0
anna.benettoni <anna.benettoni@intesys.it>
2024-12-09 09:19:39 +0100

    new session

--- frontend/src/routes/teacher/sessions/sessionComponents/NewSession.tsx
+++ frontend/src/routes/teacher/sessions/sessionComponents/NewSession.tsx
@@ -1,3 +1,32 @@
+import { Select, Option } from "@mui/joy";
+import {
+  DatePicker,
+  LocalizationProvider,
+  TimePicker,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+
 export const NewSession = () => {
-  return "ciao";
+  return (
+    <>
+      <h1>Nuova sessione di</h1>
+      <p>Assegna alla classe:</p>
+      <Select placeholder="Choose one…">
+        <Option value="dog"></Option>
+      </Select>
+      <p>Data della sessione:</p>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DatePicker"]}>
+          <DatePicker label="Basic date picker" />
+        </DemoContainer>
+      </LocalizationProvider>{" "}
+      <p>Ora di inizio:</p>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["TimePicker"]}>
+          <TimePicker label="Basic time picker" />
+        </DemoContainer>
+      </LocalizationProvider>
+    </>
+  );
 };
