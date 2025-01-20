89bc9340cba2fc1ca8482f670b3508f4a829016f
filippomeneguzzi <filippomeneguzzi30@gmail.com>
2025-01-20 11:33:41 +0100

    lezione 20 gennaio

--- frontend/src/routes/teacher/exams/examComponents/QuestionView.tsx
+++ frontend/src/routes/teacher/exams/examComponents/QuestionView.tsx
@@ -2,19 +2,36 @@ import EditIcon from "@mui/icons-material/Edit";
 import { Button, Card, Stack, Typography } from "@mui/joy";
 import React from "react";
 import { Question } from "../../../../../../api-types";
+import { QuestionForm } from "./QuestionForm";
 
-export const QuestionView: React.FC<{ question: Question; index: number }> = ({
+export const QuestionView: React.FC<{ 
+  question: Question; 
+  index: number; 
+  onSave: (index: number, question: Question) => void 
+}> = ({
   question,
   index,
+  onSave,
 }) => {
-  return (
+  const [editing, setEditing] = React.useState(false);
+  const handleSave = (question: Question) => {
+    onSave(index, question);
+    setEditing(false);
+  }
+
+  return editing 
+  ?  <QuestionForm 
+    saveQuestion={ handleSave } 
+    question={question} 
+    />
+  : (
     <Card>
       <Stack direction={"row"} justifyContent={"space-between"}>
         <Typography>
           {index + 1}) {question.text || "Domanda vuota"}
         </Typography>
-        <Button endDecorator={<EditIcon />} size="sm">
-          {/* TODO add edit question functionality  */}
+        <Button endDecorator={<EditIcon />} onClick={()=>{setEditing(!editing)}} size="sm" >
+        
           Modifica
         </Button>
       </Stack>
